4344c2afdb8ee239d03d923608b8069ddb1f2a84
Evgenia Fedorova <leleoartist@gmail.com>
2022-06-08 21:00:49 +0300

    feat: complete task 3.2

--- src/MiniMaxInputFn.js
+++ src/MiniMaxInputFn.js
@@ -1,75 +1,65 @@
-import React from 'react';
+import { useState } from 'react';
 
 const style = { display: 'inline-block', padding: '1px 5px', width: '30px' };
 
-class MiniMaxInputFn extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { curCount: props.min };
-    this.onChange = this.onChange.bind(this);
-  }
+const MiniMaxInputFn = ({ min, max }) => {
+  const [curCount, setCurCount] = useState(min);
 
-  onChange = (e) => {
+  const onChange = (e) => {
     const num = parseInt(e.target.value, 10);
-    const curValue = Number.isNaN(num) ? this.props.min : num;
+    const curValue = Number.isNaN(num) ? min : num;
 
-    if (Number(curValue) > this.props.max) return;
+    if (Number(curValue) > max) return;
 
-    this.setState(() => ({ curCount: Number(curValue) }));
+    setCurCount(Number(curValue));
   };
 
-  onBlur = (e) => {
-    if (Number(e.target.value) < this.props.min) {
-      this.setState(() => ({ curCount: this.props.min }));
+  const onBlur = (e) => {
+    if (Number(e.target.value) < min) {
+      setCurCount(min);
     }
   };
 
-  increment = () => {
-    if (this.state.curCount >= this.props.max) return;
+  const increment = () => {
+    if (curCount >= max) return;
 
-    this.setState((prevState) => ({
-      curCount: Number(prevState.curCount) + 1,
-    }));
+    setCurCount(Number(curCount) + 1);
   };
 
-  decrement = () => {
-    if (this.state.curCount <= this.props.min) return;
+  const decrement = () => {
+    if (curCount <= min) return;
 
-    this.setState((prevState) => ({
-      curCount: Number(prevState.curCount) - 1,
-    }));
+    setCurCount(Number(curCount) - 1);
   };
 
-  render() {
-    return (
-      <div>
-        <h3>Функциональный MinMax</h3>
-        <p>
-          Возможные значения: от {this.props.min} до {this.props.max}:
-        </p>
-        <button
-          type="button"
-          disabled={Number(this.state.curCount) <= this.props.min}
-          onClick={this.decrement}
-        >
-          -
-        </button>
-        <input
-          style={style}
-          value={this.state.curCount}
-          onChange={this.onChange}
-          onBlur={this.onBlur}
-        />
-        <button
-          type="button"
-          disabled={Number(this.state.curCount) >= this.props.max}
-          onClick={this.increment}
-        >
-          +
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h3>Функциональный MinMax</h3>
+      <p>
+        Возможные значения: от {min} до {max}:
+      </p>
+      <button
+        type="button"
+        disabled={Number(curCount) <= min}
+        onClick={decrement}
+      >
+        -
+      </button>
+      <input
+        style={style}
+        value={curCount}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      <button
+        type="button"
+        disabled={Number(curCount) >= max}
+        onClick={increment}
+      >
+        +
+      </button>
+    </div>
+  );
+};
 
 export default MiniMaxInputFn;
