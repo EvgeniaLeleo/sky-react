95c2a504902f5cbf621dd707451ed38a16e22ae9
Evgenia Fedorova <leleoartist@gmail.com>
2022-06-11 13:47:49 +0300

    feat: complete task 2

---
+++ sky-react-hw1/src/NumbersTable.js
@@ -0,0 +1,42 @@
+import isPrime from './utils'
+
+function NumbersTable(props) {
+  const { n } = props
+  const arr = []
+
+  for (let i = 0; i < n; i += 1) {
+    arr.push(i)
+  }
+
+  return (
+    <div className="container">
+      <div className="numbers-table">
+        {arr.map((el) => {
+          if (Number(el) % 2 === 0 && Number(el) !== 2) {
+            return (
+              <div className="cell even" key={el}>
+                {el}
+              </div>
+            )
+          }
+
+          if (isPrime(Number(el))) {
+            return (
+              <div className="cell prime" key={el}>
+                {el}
+              </div>
+            )
+          }
+
+          return (
+            <div className="cell odd" key={el}>
+              {el}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default NumbersTable
