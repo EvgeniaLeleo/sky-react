94a2b527c93d4217a5af9444fa779b4d7e71b16f
Evgenia Fedorova <leleoartist@gmail.com>
2022-06-19 19:04:14 +0300

    refactor: rename isValidEmail argument

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,5 +1,5 @@
-const isValidEmail = (string) => {
-  return string.match(/^[\w]{1}[\w-.]*@[\w-]+\.[a-z]{2,4}$/i);
+const isValidEmail = (email) => {
+  return email.match(/^[\w]{1}[\w-.]*@[\w-]+\.[a-z]{2,4}$/i);
 };
 
 export default isValidEmail;
